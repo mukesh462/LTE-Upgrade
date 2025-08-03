1fee8280ce1a82386649361027feb04eac4a8798
Mukesh <mr.mukesh@462gmail.com>
2025-08-03 23:44:17 +0530

    first commit

---
+++ NotificationService.js
@@ -0,0 +1,39 @@
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import { useEffect, useState } from 'react';
+
+export default function useFirebaseToken() {
+  const [fcmToken, setFcmToken] = useState(null);
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => {
+      console.log('FCM Token:', token);
+      setFcmToken(token);
+    });
+  }, []);
+
+  return fcmToken;
+}
+
+async function registerForPushNotificationsAsync() {
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token!');
+      return;
+    }
+
+    // This gets the **Firebase FCM token**
+    const { data: fcmToken } = await Notifications.getDevicePushTokenAsync();
+    return fcmToken;
+  } else {
+    alert('Must use a physical device');
+  }
+}
